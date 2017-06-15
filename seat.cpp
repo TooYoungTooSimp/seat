df94d7710bb33c28ac4ed253722489a2fe8b83b1
雷宇辰 <crazymonkeylyc@outlook.com>
2017-06-15 20:27:53 +0800

    Thu, 15 Jun 2017 20:27:53 +0800

--- seat.cpp
+++ seat.cpp
@@ -4,21 +4,28 @@
 using namespace std;
 mt19937 __get_random_engine();
 int num[49];
+char name[50][9];
 #define numRange num, num + 49
-const int lcy = 20, wzr = 26, lyc = 48;
-int main()
+const int lcy = 20, wzr = 26, lyc = 49, yyl = 48, hr = 8;
+int main(int argc, char **argv)
 {
-    freopen("out.txt", "w", stdout);
+    FILE *iS, *oS;
+    if (argc == 1) iS = stdin, oS = stdout;
+    if (argc == 2) iS = fopen(argv[1], "r"), oS = stdout;
+    if (argc == 3) iS = fopen(argv[1], "r"), oS = fopen(argv[2], "w");
+    for (int id; ~fscanf(iS, "%d", &id);)
+        fscanf(iS, "%s", name[id]);
     mt19937 gen = __get_random_engine();
     for (int i = 0; i < 49; i++) num[i] = i + 1;
     shuffle(numRange, gen);
     iter_swap(find(numRange, lcy), &num[(gen() & 7) | 8]);
     iter_swap(find(numRange, wzr), &num[(find(numRange, lcy) - num) ^ 1]);
+    iter_swap(find(numRange, yyl), &num[(find(numRange, hr) - num) ^ 1]);
     iter_swap(find(numRange, wzr), find(numRange, lyc));
     iter_swap(find(numRange, wzr), num + 48);
-    for (int i = 0; i < 6; i++, putchar('\n'))
-        for (int j = 0; j < 8; j++, putchar(' '))
-            printf("%2d", num[i << 3 | j]);
+    for (int i = 0; i < 6; i++, fputc('\n', oS))
+        for (int j = 0; j < 8; j++, fputc(' ', oS))
+            fprintf(oS, "%-8s%c", name[num[i << 3 | j]], " |"[j & 1]);
     return 0;
 }
 #ifdef _WIN32
