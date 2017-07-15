5ea547cae0781dc2bf45e29926ee996ff4d11272
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2017-07-15 13:50:29 +0000

    Sat, 15 Jul 2017 13:50:28 +0000

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 一个用来给22班随机排座位表的程序。因为他们班的同学认为@hr567的程序有问题，随机的不好，就让我来重写一个。
 
+## 原有功能
 现在seat.cpp已经实现seat_hr.cpp中的所有功能并且有着更好的随机性。
 seat.cpp使用CryptGenRandom(Windows)或是/dev/random(unix-like)产生的高质量的随机数种子来初始化mt19937，达到了比srand(time(0))+rand()更好的随机性。
 
@@ -9,8 +10,12 @@ seat.cpp使用CryptGenRandom(Windows)或是/dev/random(unix-like)产生的高质
 - 把lcy放到第二排
 - 把wzr放到lcy旁边
 
+## Update 20170701
 两件事情
 - 表哥（wzr）回来了
 - 22班座位安排方法进行了大的改动
 
 于是实现新功能：按照给定的固定同桌表安排一些人，剩下的人随机分配。
+
+##Update 20170715
+固定同桌表的方式进行了更新，直接重写算了
--- seat.cpp
+++ seat.cpp
@@ -13,20 +13,18 @@ int main(int argc, char **argv)
         fputs("Usage: seat name.txt fixed.txt [result.txt]\n", stderr);
         fputs("name.txt is a file that gives a map of id and name.\n", stderr);
         fputs("fixed.txt is a file that gives several pairs of fixed deskmates by their ids.\n", stderr);
-        fputs("Optional result.txt is a file where this program will write result. \n    Otherwise the result will be write to stdout.", stderr);
+        fputs("Optional result.txt is a file where this program will write result. \n    Otherwise the result will be write to stdout.\n", stderr);
         return 1;
     }
     FILE *nStream, *fStream, *rStream;
     nStream = fopen(argv[1], "r"), fStream = fopen(argv[2], "r");
     rStream = argc == 4 ? fopen(argv[3], "r") : stdout;
-    array<int, 48> res;
-    array<char[9], 49> name;
+    vector<int> res;
+    array<char[9], 50> name;
     vector<pair<int, int>> fixed;
     //read
-    const int lcy = 20, yyl = 48, hr = 8;
     for (int id; ~fscanf(nStream, "%d", &id);) fscanf(nStream, "%s", name[id]);
     for (int x, y; ~fscanf(fStream, "%d%d", &x, &y);) fixed.push_back(make_pair(x, y));
-    fixed.push_back(make_pair(yyl, hr));
     //check
     array<bool, 49> H;
     fill(H.begin(), H.end(), false);
@@ -40,15 +38,23 @@ int main(int argc, char **argv)
             H[ite->first] = H[ite->second] = true;
     //generate
     mt19937 gen = __get_random_engine();
-    for (int i = 0; i < 48; i++) res[i] = i + 1;
+    for (int i = 1; i <= 48; i++)
+        if (!H[i]) res.push_back(i);
     shuffle(res.begin(), res.end(), gen);
-    iter_swap(find(res.begin(), res.end(), lcy), &res[(gen() & 7) | 8]); //Special: lcy is on row 2
+    shuffle(fixed.begin(), fixed.end(), gen);
     for (vector<pair<int, int>>::iterator ite = fixed.begin(); ite != fixed.end(); ++ite)
-        iter_swap(find(res.begin(), res.end(), ite->first), &res[(find(res.begin(), res.end(), ite->second) - res.begin()) ^ 1]);
+        if (gen()) swap(ite->first, ite->second);
+    //special
+    iter_swap(find(res.begin(), res.end(), 48), &res[(find(res.begin(), res.end(), 8) - res.begin()) ^ 1]);
+    iter_swap(find_if(fixed.begin(), fixed.end(), [](auto &p) { return min(p.first, p.second) == 20; }), fixed.begin() + (gen() & 1 | 3));
     //display
     for (int i = 0; i < 6; i++, fputs("|\n", rStream))
-        for (int j = 0; j < 8; j++)
-            fprintf(rStream, "%s%-8s", &"| "[j & 1], name[res[i << 3 | j]]);
+    {
+        for (int j = 0; j < 4; j++)
+            fprintf(rStream, "%s%-8s", &"| "[j & 1], name[res[i << 2 | j]]);
+        for (int j = 0; j < 2; j++)
+            fprintf(rStream, "| %-8s %-8s", name[fixed[i << 1 | j].first], name[fixed[i << 1 | j].second]);
+    }
     fclose(nStream), fclose(fStream), fclose(rStream);
     return 0;
 }
